33dfa8cc9dcfe7628880958f52c0fed10aaf6a3d
Shubham-Rasal <bluequbits@gmail.com>
2021-12-14 17:37:22 +0530

    remaining files

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+// async function request()
+// {
+//     console.log("ello");
+// const response= await fetch('data.csv')
+// // console.log(response)
+// const data = await response.text()
+// const table =data.split("\n").slice(1)
+// // console.log(table)
+// table.forEach(element => {
+//     const cols=element.split(',')
+//     // console.log(cols)
+//     if(cols[0]=="GCAG")
+//     {
+//     const gcag=cols[0];
+//     const gsagmean=cols[2];
+//     // console.log(gsagmean+"this is gsag")
+//     }
+//     if(cols[0]=="GISTEMP")
+//     {
+//     const gistemp=cols[0];
+//     const gistmean=cols[2];
+//     }
+//     const year=cols[1];
+//     xlables.push(year);
+   
+//     // console.log(year)
+// });
+// // console.log(table[1])
+// }
